88b5a87d28d78d57d63061ecb45158c24a616be7
Shelby Level <16slevel@gmail.com>
2024-01-31 12:09:50 -0500

    changed size of navbar logo

--- logos/hexsticker.R
+++ logos/hexsticker.R
@@ -7,4 +7,4 @@ font_add_google("Nunito Sans", "Montserrat")
 showtext_auto()
 
 imgurl <- system.file("star-icon.svg", package="Shelby Level")
-sticker("logos/black-sl-logo.png", package= NULL, p_size=20, s_x=1, s_y=.85, s_width=.9, s_height=.9, h_fill="#7fa6ad", h_color="#45767a", p_family = 'Montserrat', filename="logos/sl_sticker.png")
+sticker("logos/black-sl-logo-crop.png", package= NULL, p_size=20, s_x=1.01, s_y=1, s_width=.8, s_height=.9, h_fill="#7fa6ad", h_color="#45767a", p_family = 'Montserrat', filename="logos/sl_sticker.png")
